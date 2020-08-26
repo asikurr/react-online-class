b8168066a85e4d5a04e3ce5a0742f6908f790293
Asikur <asikurrahman7@gmail.com>
2020-08-26 23:57:43 +0600

    completed json fake data for online class project.

---
+++ src/components/Course/Course.jsx
@@ -0,0 +1,14 @@
+import React , {useState}from 'react';
+import CourseData from '../../courseData/courseData.js'
+
+const Course = () => {
+    const [course, setCourse] = useState(CourseData);
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Course;
---
+++ src/courseData/courseData.js
@@ -0,0 +1,155 @@
+import imgOne from './course-img/1.jpg'
+import imgTwo from './course-img/2.jpg'
+import imgThree from './course-img/3.jpg'
+import imgFour from './course-img/4.jpg'
+import imgFive from './course-img/5.jpg'
+import imgSix from './course-img/6.jpg'
+import imgSeven from './course-img/7.jpg'
+import imgEight from './course-img/8.jpg'
+import imgNine from './course-img/9.jpg'
+import imgTen from './course-img/10.jpg'
+import imgEleven from './course-img/11.jpg'
+import imgTwelve from './course-img/12.jpg'
+import imgThirteen from './course-img/13.jpg'
+import imgFourteen from './course-img/14.jpg'
+import imgFifteen from './course-img/15.jpg'
+
+
+const courseDataList = [
+    {
+        "id": 1,
+        "courseName": "Advanced CSS and Sass: Flexbox, Grid, Animations and More!",
+        "instructorName": "Jonas Schmedtmann",
+        "email": "Sincere@april.biz",
+        "imgUrl":imgOne,
+        "coursePrice" : 170,
+       
+    },
+    {
+        "id": 2,
+        "courseName": "The Complete JavaScript Course 2020: Build Real Projects!",
+        "instructorName": "Jonas Schmedtmann",
+        "email": "Shanna@melissa.tv",
+        "imgUrl":imgTwo,
+        "coursePrice" : 150,
+        
+    },
+    {
+        "id": 3,
+        "courseName": "React - The Complete Guide (incl Hooks, React Router, Redux)",
+        "instructorName": "Academind by Maximilian Schwarzmüller",
+        "email": "Nathan@yesenia.net",
+        "imgUrl":imgThree,
+        "coursePrice" : 140,
+        
+    },
+    {
+        "id": 4,
+        "courseName": "The Complete React Native + Hooks Course [2020 Edition]",
+        "instructorName": "Stephen Grider",
+        "email": "Julianne.OConner@kory.org",
+        "imgUrl": imgFour,
+        "coursePrice" : 114,
+       
+    },
+    {
+        "id": 5,
+        "courseName": "Artificial Intelligence A-Z™: Learn How To Build An AI",
+        "instructorName": "Hadelin de Ponteves, Kirill Eremenko",
+        "email": "Lucio_Hettinger@annie.ca",
+        "imgUrl":imgFive,
+        "coursePrice" : 107,
+        
+    },
+    {
+        "id": 6,
+        "courseName": "JavaScript Understanding the Weird Parts",
+        "instructorName": "Anthony Alicea",
+        "email": "Karley_Dach@jasper.info",
+        "imgUrl":imgSix,
+        "coursePrice" : 150,
+       
+    },
+    {
+        "id": 7,
+        "courseName": "iOS 10 & Swift 3: From Beginner to Paid Professional™",
+        "instructorName": "Mark Wahlbeck, Devslopes by Mark Wahlbeck",
+        "email": "Telly.Hoeger@billy.biz",
+        "imgUrl":imgSeven,
+        "coursePrice" : 135,
+       
+    },
+    {
+        "id": 8,
+        "courseName": "Complete Python Bootcamp: From Zero to Hero in Python",
+        "instructorName": "Jose Portilla",
+        "email": "Sherwood@rosamond.me",
+        "imgUrl":imgEight,
+        "coursePrice" : 122,
+       
+    },
+    {
+        "id": 9,
+        "courseName": "The Web Developer Bootcamp",
+        "instructorName": "Colt Steele",
+        "email": "Chaim_McDermott@dana.io",
+        "imgUrl":imgNine,
+        "coursePrice" : 111,
+        
+    },
+    {
+        "id": 10,
+        "courseName": "Java Programming Masterclass for Software Developers",
+        "instructorName": "Tim Buchalka, Tim Buchalka's",
+        "email": "Rey.Padberg@karina.biz",
+        "imgUrl":imgTen,
+        "coursePrice" : 95,
+        
+    },
+    {
+        "id": 11,
+        "courseName": "The Complete Node.js Developer Course (3rd Edition)",
+        "instructorName": "Andrew Mead, Rob Percival",
+        "email": "Francis.Conway@karina.biz",
+        "imgUrl":imgEleven,
+        "coursePrice" : 130,
+       
+    },
+    {
+        "id": 12,
+        "courseName": "Vue JS - The Complete Guide (incl. Vue Router & Vuex)",
+        "instructorName": "Maximilian Schwarzmüller",
+        "email": "Francis.Conway@karina.biz",
+        "imgUrl":imgTwelve,
+        "coursePrice" : 115,
+       
+    },
+    {
+        "id": 13,
+        "courseName": "R Programming A-Z™: R For Data Science With Real Exercises!",
+        "instructorName": "Kirill Eremenko, SuperDataScience Team",
+        "email": "Richard.F.Bridges@karina.biz",
+        "imgUrl":imgThirteen,
+        "coursePrice" : 95,
+       
+    }
+    ,{
+        "id": 14,
+        "courseName": "Python and Django Full Stack Web Developer Bootcamp",
+        "instructorName": "Jose Portilla",
+        "email": "Rogelio.D.Deboer@karina.biz",
+        "imgUrl":imgFourteen,
+        "coursePrice" : 105,
+        
+    },
+    {
+        "id": 15,
+        "courseName": "MongoDB - The Complete Developer's Guide 2020",
+        "instructorName": "Maximilian Schwarzmüller",
+        "imgUrl":imgFifteen,
+        "coursePrice" : 110,
+
+    }
+]
+
+export default courseDataList;
