112b2ccd8336a7c27e6948d6c134ec7cef9ea9fd
Asikur <asikurrahman7@gmail.com>
2020-08-27 14:32:22 +0600

    fixed some problem.

--- src/components/cartCourse/CartCourse.jsx
+++ src/components/cartCourse/CartCourse.jsx
@@ -6,7 +6,7 @@ const CartCourse = (props) => {
     const totalPrice = cart.reduce((total , cart) => total + cart.coursePrice , 0)
     return (
         <div className="d-flex flex-column">
-            <h4 className="bg-success border p-2">Number of Enrolled : {cart.length}</h4>
+            <h4 className="bg-success border p-2">Course Enrolled : {cart.length}</h4>
             <h5 className="bg-danger p-2 border">Total Price : $ {totalPrice}</h5>
         </div>
     );
